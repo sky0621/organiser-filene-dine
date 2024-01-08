6e07f4cbda922b5be216d9a3a2ca7346b642f889
sky0621 <sky14work@gmail.com>
2024-01-08 14:15:02 +0900

    -

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 	if rename == "0" {
 		isRename = false
 	}
+	log.Println(isRename)
 	targetExts := strings.Split(os.Args[4], ",")
 
 	fileList, err := os.Create(filepath.Join(toDir, fileListName))
@@ -50,12 +51,14 @@ func main() {
 
 	if err := filepath.WalkDir(fromDir, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
+			log.Println("[ 01 ]")
 			log.Println(err)
 			return err
 		}
 
 		fi, err := d.Info()
 		if err != nil {
+			log.Println("[ 02 ]")
 			log.Println(err)
 			return nil
 		}
@@ -71,33 +74,29 @@ func main() {
 
 		log.Println(path)
 
-		return exec(path, existsSet, toDir, fi, isRename)
+		return exec(path, existsSet, toDir, fi, isRename, fileList)
 	}); err != nil {
 		log.Fatal(err)
 	}
 
-	for _, s := range existsSet.ToSlice() {
-		_, err := fileList.WriteString(fmt.Sprintf("%s\n", s))
-		if err != nil {
-			log.Println(err)
-		}
-	}
 }
 
-func exec(path string, existsSet mapset.Set[string], toDir string, fi fs.FileInfo, isRename bool) error {
+func exec(path string, existsSet mapset.Set[string], toDir string, fi fs.FileInfo, isRename bool, fileList *os.File) error {
 	name := fi.Name()
 	createdTime := getCreatedTime(fi)
 	size := fi.Size()
 
-	if !addExists(existsSet, createExistsSetElement(name, formatTime(createdTime), size)) {
+	element := createExistsSetElement(name, formatTime(createdTime), size)
+	if !addExists(existsSet, element) {
 		return nil
 	}
 
 	/*
 	 * Output Directory
 	 */
-	outDirName := getOutputDirName(createdTime)
+	outDirName := getOutputDirName(path)
 	if err := createOutputDir(toDir, outDirName); err != nil {
+		log.Println("[[[ 01 ]]]")
 		log.Println(err)
 		return nil
 	}
@@ -111,11 +110,13 @@ func exec(path string, existsSet mapset.Set[string], toDir string, fi fs.FileInf
 	}
 	outFile, err := os.Create(filepath.Join(toDir, outDirName, outFileName))
 	if err != nil {
+		log.Println("[[[ 02 ]]]")
 		log.Println(err)
 		return nil
 	}
 	defer func() {
 		if err := outFile.Close(); err != nil {
+			log.Println("[[[ 03 ]]]")
 			log.Println(err)
 		}
 	}()
@@ -125,11 +126,13 @@ func exec(path string, existsSet mapset.Set[string], toDir string, fi fs.FileInf
 	 */
 	f, err := os.Open(path)
 	if err != nil {
+		log.Println("[[[ 04 ]]]")
 		log.Println(err)
 		return nil
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
+			log.Println("[[[ 05 ]]]")
 			log.Println(err)
 		}
 	}()
@@ -138,8 +141,14 @@ func exec(path string, existsSet mapset.Set[string], toDir string, fi fs.FileInf
 	 * Copy
 	 */
 	if _, err := io.Copy(outFile, f); err != nil {
+		log.Println("[[[ 06 ]]]")
 		log.Println(err)
 		return nil
+	}
+
+	_, err = fileList.WriteString(fmt.Sprintf("%s\n", element))
+	if err != nil {
+		log.Println(err)
 	}
 
 	return nil
@@ -151,9 +160,7 @@ func createExistsSetElement(name string, birthTime string, size int64) string {
 
 func addExists(existsSet mapset.Set[string], element string) bool {
 	if existsSet.Contains(element) {
-		log.Println("=====================")
-		log.Println("EXISTS:", element)
-		log.Println("=====================")
+		log.Println("[EXISTS]:", element)
 		return false
 	}
 	existsSet.Add(element)
@@ -172,8 +179,19 @@ func createOutFileName(createdTime time.Time, size int64, ext string) string {
 	return fmt.Sprintf("%s_%d%s", formatTime(createdTime), size, ext)
 }
 
-func getOutputDirName(t time.Time) string {
-	return t.Format("200601")
+func getOutputDirName(path string) string {
+	dir, _ := filepath.Split(path)
+	//log.Println(dir)
+	dirs := strings.Split(dir, "/")
+	//dirs := filepath.SplitList(dir)
+	//log.Println(dirs)
+	//log.Println(len(dirs))
+	if len(dirs) < 2 {
+		return "root"
+	}
+	ret := dirs[len(dirs)-2]
+	//log.Println(ret)
+	return ret
 }
 
 func createOutputDir(toDir string, outDirName string) error {
